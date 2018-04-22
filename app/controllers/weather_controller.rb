36f927661b2161fdada7eb23a48c7a644fedb418
pyama86 <pyama@pepabo.com>
2018-04-22 16:06:25 +0900

    use value

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,9 +1,8 @@
 class WeatherController < ApplicationController
   def create
-    request = AlexaRubykit::build_request(params)
-    response = AlexaRubykit::Response.new
-
-    response.add_speech("こんにちは")
+    # インテントからcityの文字列を受け取る
+    city = request.slots[:city][:value]
+    response.add_speech("#{city}を受け取りました")
     render json: response.build_response
   end
 end
